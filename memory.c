bc0cfdca64f15f02e0887559631577cb520c1ccd
codeabuu <abdulngui718@gmail.com>
2023-05-13 15:45:12 -0700

    memory code

--- memory.c
+++ memory.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * bfree - frees a pointer and NULLs the address
- * @pointer: address of the pointer to free
+ * bfree - will free a pointer & NULLs the address
+ * @pointer: addr of ptr to free
  *
  * Return: 1 if freed, otherwise 0.
  */
