884930df8429d2467429f563f22856b8c16ccdcb
lawrence1986 <madusquare@gmail.com>
2023-05-13 18:57:15 -0700

    Updated shell.h

--- shell.h
+++ shell.h
@@ -115,7 +115,7 @@ int find_builtin(info_t *);
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
 
-/* toem_parser.c */
+/* toem_parsing.c */
 int is_cmd(info_t *, char *);
 char *duplicate_chars(char *, int, int);
 char *find_route(info_t *, char *, char *);
@@ -123,81 +123,81 @@ char *find_route(info_t *, char *, char *);
 /* loophsh.c */
 int loophsh(char **);
 
-/* toem_errors.c */
+/* toem_error_handling.c */
 void _eputs(char *);
 int _eputchar(char);
 int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
 
-/* toem_string.c */
+/* toem_text_operations.c */
 int _strlen(char *);
 int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
 char *_strcat(char *, char *);
 
-/* toem_string1.c */
+/* toem_text1_operations.c */
 char *_strcpy(char *, char *);
 char *_strdup(const char *);
 void _puts(char *);
 int _putchar(char);
 
-/* toem_exits.c */
+/* toem_exit_handling.c */
 char *_strncpy(char *, char *, int);
 char *_strncat(char *, char *, int);
 char *_strchr(char *, char);
 
-/* toem_tokenizer.c */
+/* toem_lexer.c */
 char **strtow(char *, char *);
 char **strtow2(char *, char);
 
-/* toem_realloc.c */
+/* toem_reallocation.c */
 char *_setmem(char *, char, unsigned int);
 void ffree(char **);
-void *_realloc(void *, unsigned int, unsigned int);
+void *_reallocation(void *, unsigned int, unsigned int);
 
-/* toem_memory.c */
+/* toem_stack.c */
 int bfree(void **);
 
-/* toem_atoi.c */
+/* toem_atoi_int.c */
 int interact(info_t *);
 int is_delimeter(char, char *);
 int _isalpha(int);
-int _atoi(char *);
+int _atoi_int(char *);
 
-/* toem_errors1.c */
+/* toem_error1_handling.c */
 int _eratoi(char *);
 void print_err(info_t *, char *);
 int print_d(int, int);
 char *conv_no(long int, int, int);
 void rm_comments(char *);
 
-/* toem_builtin.c */
+/* toem_inline.c */
 int _myquit(info_t *);
 int _mycdir(info_t *);
 int _myassist(info_t *);
 
-/* toem_vars.c */
+/* toem_variable_utils.c */
 int is_chain(info_t *, char *, size_t *);
 void check_chain(info_t *, char *, size_t *, size_t, size_t);
 int replace_alias(info_t *);
 int replace_variables(info_t *);
 int replace_str(char **, char *);
 
-/* toem_builtin1.c */
+/* toem_inline1.c */
 int _myprev(info_t *);
 int _myalias(info_t *);
 
-/*toem_getline.c */
+/*toem_get_Line.c */
 ssize_t get_input(info_t *);
-int _getline(info_t *, char **, size_t *);
+int _get_Line(info_t *, char **, size_t *);
 void sigintHandler(int);
 
-/* toem_getinfo.c */
+/* toem_user_input.c */
 void clear_data(info_t *);
 void set_data(info_t *, char **);
 void free_data(info_t *, int);
 
-/* toem_environ.c */
+/* toem_environment.c */
 char *_getenviron(info_t *, const char *);
 int _myenvironment(info_t *);
 int _mysetenviron(info_t *);
@@ -209,21 +209,21 @@ char **get_environ(info_t *);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
 
-/* toem_history.c */
+/* toem_narrative.c */
 char *get_history_file(info_t *info);
 int write_history(info_t *info);
 int read_history(info_t *info);
 int build_history_list(info_t *info, char *buf, int linecount);
 int renumber_history(info_t *info);
 
-/* toem_lists.c */
+/* toem_linked_lists.c */
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
 size_t print_ls_string(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 
-/* toem_lists1.c */
+/* toem_linked_lists1.c */
 size_t list_len(const list_t *);
 char **list_to_strings(list_t *);
 size_t print_list(const list_t *);
