727d1c1aceac63dc096ea98b5859d36e7603f58a
lawrence1986 <madusquare@gmail.com>
2023-05-15 06:02:27 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -53,19 +53,19 @@ typedef struct liststr
 * struct passinfo - contains pseudo-arguements to pass into a function,
 * allowing uniform prototype for function pointer struct
 * @arg: a string generated from getline containing arguements
-* @argv:an array of strings generated from arg
+* @argv:an array of strings generated from arguements
 * @path: a string path for the current command
 * @argc: the argument count
 * @line_count: the error count
 * @err_num: the error code for exit()s
 * @linecount_flag: if on count this line of input
 * @fname: the program filename
-* @env: linked list local copy of environ
+* @env: linked list local copy of environment
 * @environ: custom modified copy of environ from LL env
 * @history: the history node
 * @alias: the alias node
-* @env_changed: on if environ was changed
-* @status: the return status of the last exec'd command
+* @env_changed: on if environment was changed
+* @status: the return status of the last executed command
 * @cmd_buf: address of pointer to cmd_buf, on if chaining
 * @cmd_buf_type: CMD_type ||, &&, ;
 * @readfd: the fd from which to read line input
