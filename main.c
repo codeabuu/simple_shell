6f6c2f00c75a48c7b30afe561c6151310cebabd7
codeabuu <abdulngui718@gmail.com>
2023-05-23 17:12:37 -0700

    main

--- main.c
+++ main.c
@@ -2,45 +2,44 @@
 
 /**
  * main - entry point
-* @argc: arg count
-* @argv: arg vector
-*
-* Return: 0 on success, 1 on error
-*/
-
-int main(int argc, char **argv)
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
 {
-	info_t info = INFO_INIT;
+	info_t info[] = { INFO_INIT };
 	int fd = 2;
 
-	__asm__ ("mov %1, %0\n\t"
+	asm ("mov %1, %0\n\t"
 			"add $3, %0"
 			: "=r" (fd)
 			: "r" (fd));
 
-	if (argc == 2)
+	if (ac == 2)
 	{
-		fd = open(argv[1], O_RDONLY);
+		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(argv[0]);
+				_eputs(av[0]);
 				_eputs(": 0: Can't open ");
-				_eputs(argv[1]);
+				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
-		info.readfd = fd;
+		info->readfd = fd;
 	}
-	populate_env_list(&info);
-	read_history(&info);
-	hsh(&info, argv);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
 
